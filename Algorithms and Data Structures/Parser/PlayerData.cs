5ff6223e7f77e543e020acfb3f0c3785fd9a7e26
Tara Karimi <tara.karimi@icloud.com>
2023-10-09 17:24:57 +0330

    Player data class created

---
+++ Algorithms and Data Structures/Parser/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PlayerData
+{
+    public string Username { get; set; }
+    public int Level { get; set; }
+    public string Guild { get; set; }
+    public List<string> Items { get; set; }
+    public string SkinId { get; set; }
+}
