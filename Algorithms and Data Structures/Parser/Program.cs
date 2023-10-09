074ab8d8856d9aa9c12e6e89cc6ab9c3b34fd1da
Tara Karimi <tara.karimi@icloud.com>
2023-10-09 17:27:37 +0330

    Read Json from file and store in dictionary

---
+++ Algorithms and Data Structures/Parser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        string jsonFilePath = "E:/Medrick-Projects/JsonParser/JsonParser/data.json";
+        string jsonString = File.ReadAllText(jsonFilePath);
+
+        List<PlayerData> players = JsonConvert.DeserializeObject<List<PlayerData>>(jsonString);
+        Dictionary<string, PlayerData> playerDataDictionary = new Dictionary<string, PlayerData>();
+        PlayerData selectedPlayerData;
+
+        foreach (var player in players)
+        {
+            playerDataDictionary.Add(player.Username, player);
+        }
+    }
+}
