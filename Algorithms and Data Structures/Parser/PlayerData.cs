6031e42e2815102a08d0b29be65205ac700483e4
Tara Karimi <tara.karimi@icloud.com>
2023-10-09 17:26:25 +0330

    Display method

--- Algorithms and Data Structures/Parser/PlayerData.cs
+++ Algorithms and Data Structures/Parser/PlayerData.cs
@@ -17,4 +17,27 @@ public class PlayerData
         Items = items;
         SkinId = skinId;
     }
+
+    public void Display()
+    {
+        Console.WriteLine($"Username: {Username}");
+        Console.WriteLine($"Level: {Level}");
+        Console.WriteLine($"Guild: {Guild}");
+        Console.WriteLine("Items:");
+
+        if (Items != null && Items.Count > 0)
+        {
+            foreach (var item in Items)
+            {
+                Console.WriteLine($"  {item}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("  null");
+        }
+
+        Console.WriteLine($"Skin ID: {SkinId}");
+    }
+
 }
