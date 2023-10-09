37938d18f3065743af912cc3a9e5c97ab5c99f8e
Tara Karimi <tara.karimi@icloud.com>
2023-10-09 17:16:35 +0330

    HW1 exercises completed

---
+++ Algorithms and Data Structures/HW1/HW1-Exercise4/HW1-Exercise4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end;
+            string input = "";
+            int operationNeeded = 0; 
+            
+            Console.Write("Enter the number of houses on the street: ");
+            int housesOnStreet = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the sequence for safe houses(P) and unsafe houses(H) : "); 
+            
+            while (input.Length < housesOnStreet)
+            {
+                char letter = char.ToUpper(Console.ReadKey().KeyChar);
+                if (letter == 'P' || letter == 'H')
+                {
+                    input += letter;
+                }
+                else
+                {
+                    Console.WriteLine("The entered letter is invalid. Please try again.");
+                }
+            }
+            
+            Console.WriteLine();
+
+
+            Console.Write("Please enter the start point (between 1 and {0}): ", housesOnStreet);
+            start = int.Parse(Console.ReadLine()) - 1;
+                
+            Console.Write("Please enter the end point (between 1 and {0}): ", housesOnStreet);
+            end = int.Parse(Console.ReadLine()) - 1;
+                
+            
+            bool hFound = false;
+            if (start <= end)
+            {
+                for (int i = start; i <= end; i++) 
+                {
+                    if (input[i] == 'H')
+                    {
+                        if (!hFound)
+                        {
+                            operationNeeded++;
+                            hFound = true;
+                        }
+                    }
+                    else
+                    {
+                        hFound = false;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = start; i >= end; i--) 
+                {
+                    if (input[i] == 'H')
+                    {
+                        if (!hFound)
+                        {
+                            operationNeeded++;
+                            hFound = true;
+                        }
+                    }
+                    else
+                    {
+                        hFound = false;
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Between the start point {0} and the end point {1}, there will be {2} operations needed.", start, end, operationNeeded);
+        }
+}
---
+++ Algorithms and Data Structures/HW1/HW1-Exercise5/HW1-Exercise5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number and the kTh permutation num: ");
+        
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+        int n = int.Parse(inputArray[0]);
+        int k = int.Parse(inputArray[1]);
+
+        // Generate the list of numbers from 1 to n
+        int[] numbers = Enumerable.Range(1, n).ToArray(); // linq required
+
+        // Generate kth permutation and print each one
+        PrintPermutations(numbers, k);
+    }
+
+    static void PrintPermutations(int[] numbers, int k)
+    {
+        int[] result = numbers;
+        for (int i = 1; i < k; i++)
+        {
+            result = NextPermutation(result);
+        }
+        Console.WriteLine(string.Join("", result));
+    }
+
+    static int[] NextPermutation(int[] array)
+    {
+        // Find the rightmost element smaller than its next element
+        int i = array.Length - 1;
+        while (i > 0 && array[i - 1] >= array[i])
+        {
+            i--;
+        }
+
+        Console.WriteLine("i = : " + i);
+        // if it's the last permutation
+        if (i <= 0)
+        {
+            return array;
+        }
+        
+        // Find the smallest element to the right of i-1 that is larger than i-1
+        int j = array.Length - 1;
+        while (array[j] <= array[i - 1])
+        {
+            j--;
+        }
+        Console.WriteLine("j = " + j);
+
+        // Swap values at positions i-1 and j
+        int temp = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = temp;
+
+        // Reverse the suffix
+        j = array.Length - 1;
+        while (i < j)
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+
+        return array;
+    }
+}
