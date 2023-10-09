57caeaa05db23318ee7da4537685149380924379
Tara Karimi <tara.karimi@icloud.com>
2023-10-09 17:25:49 +0330

    PlayerData constructor created + Make standard

--- Algorithms and Data Structures/Parser/PlayerData.cs
+++ Algorithms and Data Structures/Parser/PlayerData.cs
@@ -8,4 +8,13 @@ public class PlayerData
     public string Guild { get; set; }
     public List<string> Items { get; set; }
     public string SkinId { get; set; }
+
+    public PlayerData(string username = "defaultUser", int level = 0, string guild = null, List<string> items = null, string skinId = null)
+    {
+        Username = username;
+        Level = level;
+        Guild = guild;
+        Items = items;
+        SkinId = skinId;
+    }
 }
