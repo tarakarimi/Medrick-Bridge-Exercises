871a33faa345203bc561c6462c2c11d4718f13a5
Tara Karimi <tara.karimi@icloud.com>
2023-10-10 19:18:32 +0330

    Refactored program class to become more modular

--- Algorithms and Data Structures/TreasureHunt/TreasureHunt/Program.cs
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Program.cs
@@ -13,47 +13,62 @@ class Program
         
         Console.WriteLine("***Welcome to Treasure Hunt!***");
         
+        // The game loop
         while (true)
         {
-            Console.WriteLine($"Moves left: {player.Moves}");
-            grid.PrintMap(player.Row, player.Col, false);
-
-            Console.WriteLine("Enter direction (up/down/left/right) or 'cheat' to find the treasure:");
+            PrintGameInfo(player, grid);
+            
             string input = Console.ReadLine();
-
+            
             if (input.ToLower() == "cheat")
             {
-                Console.WriteLine($"Treasure is located at [{grid.TreasureRow},{grid.TreasureCol}]");
-                
-                // Also player gets to see the Treasure location visually on map
-                grid.PrintMap(player.Row, player.Col, true);
-                
-                Console.WriteLine();
+                HandleCheatCommand(player,grid);
             }
             else
             {
                 player.Move(input, rows, cols);
 
-                // Check for game outcomes
-                if (player.Row == grid.TreasureRow && player.Col == grid.TreasureCol)
-                {
-                    Console.WriteLine("***Congratulations! You found the treasure!***");
-                    Environment.Exit(0);
-                }
-
-                if (player.Row == grid.DeathTrapRow && player.Col == grid.DeathTrapCol)
-                {
-                    Console.WriteLine("***You stepped on a trap and died!***");
-                    Environment.Exit(0);
-                }
-
-                if (player.Moves == 0)
-                {
-                    Console.WriteLine("***You ran out of moves and died!***");
-                    grid.PrintMap(player.Row, player.Col, true);
-                    Environment.Exit(0);
-                }
+                CheckGameOutcomes(player, grid);
             }
+        }
+    }
+    
+    static void PrintGameInfo(Player player, Grid grid)
+    {
+        Console.WriteLine($"Moves left: {player.Moves}");
+        grid.PrintMap(player.Row, player.Col, false);
+        Console.WriteLine("Enter direction (up/down/left/right) or 'cheat' to find the treasure:");
+    }
+    
+    static void HandleCheatCommand(Player player, Grid grid)
+    {
+        Console.WriteLine($"Treasure is located at [{grid.TreasureRow},{grid.TreasureCol}]");
+                
+        // Also player gets to see the Treasure location visually on map
+        grid.PrintMap(player.Row, player.Col, true);
+                
+        Console.WriteLine();
+    }
+    
+    static void CheckGameOutcomes(Player player, Grid grid)
+    {
+        if (player.Row == grid.TreasureRow && player.Col == grid.TreasureCol)
+        {
+            Console.WriteLine("***Congratulations! You found the treasure!***");
+            Environment.Exit(0);
+        }
+
+        if (player.Row == grid.DeathTrapRow && player.Col == grid.DeathTrapCol)
+        {
+            Console.WriteLine("***You stepped on a trap and died!***");
+            Environment.Exit(0);
+        }
+
+        if (player.Moves == 0)
+        {
+            Console.WriteLine("***You ran out of moves and died!***");
+            grid.PrintMap(player.Row, player.Col, true);
+            Environment.Exit(0);
         }
     }
 }
