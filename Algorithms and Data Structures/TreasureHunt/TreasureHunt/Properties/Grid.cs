a9f7bd8e1b46895c9832e70db0907e420ca57971
Tara Karimi <tara.karimi@icloud.com>
2023-10-10 12:52:56 +0330

    Added constructor to Grid class

--- Algorithms and Data Structures/TreasureHunt/TreasureHunt/Properties/Grid.cs
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Properties/Grid.cs
@@ -10,4 +10,10 @@ class Grid
     public int TreasureCol { get; private set; }
     public int DeathTrapRow { get; private set; }
     public int DeathTrapCol { get; private set; }
+
+    public Grid(int rows, int cols)
+    {
+        Rows = rows;
+        Cols = cols;
+    }
 }
