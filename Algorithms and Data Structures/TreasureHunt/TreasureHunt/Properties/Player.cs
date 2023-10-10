5d803d78aa45b704e27be2a2e5e98aea48c07d22
Tara Karimi <tara.karimi@icloud.com>
2023-10-10 13:09:55 +0330

    Constructor added to player class

--- Algorithms and Data Structures/TreasureHunt/TreasureHunt/Properties/Player.cs
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Properties/Player.cs
@@ -5,4 +5,18 @@ class Player
     public int Row { get; private set; }
     public int Col { get; private set; }
     public int Moves { get; private set; }
+
+    public Player(Grid grid, int moves)
+    {
+        Random random = new Random();
+        
+        // To make sure the player position isn't set to [0,0], I used do while instead of while.
+        do
+        {
+            Row = random.Next(0, grid.Rows);
+            Col = random.Next(0, grid.Cols);
+        } while (Row == grid.TreasureRow && Col == grid.TreasureCol || Row == grid.DeathTrapRow && Col == grid.DeathTrapCol);
+        
+        Moves = moves;
+    }
 }
