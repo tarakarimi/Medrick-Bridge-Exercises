20980324d6578b284a401f896014d44203b6c712
Tara Karimi <tara.karimi@icloud.com>
2023-10-10 13:31:39 +0330

    Create the game map and player

---
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5;
+        int cols = 8;
+        int moves = 10;
+        bool isGameOver = false;
+
+        Grid grid = new Grid(rows, cols);
+        Player player = new Player(grid, moves);
+        
+        Console.WriteLine("***Welcome to Treasure Hunt!***");
+    }
+}
