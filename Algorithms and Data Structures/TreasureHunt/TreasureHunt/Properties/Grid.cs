f21eb87d1d2db801f4ca98b2761bff30c8142109
Tara Karimi <tara.karimi@icloud.com>
2023-10-10 12:51:41 +0330

    Grid class created

---
+++ Algorithms and Data Structures/TreasureHunt/TreasureHunt/Properties/Grid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Grid
+{
+    private int[,] map;
+    private Random random = new Random();
+    public int Rows { get; private set; }
+    public int Cols { get; private set; }
+    public int TreasureRow { get; private set; }
+    public int TreasureCol { get; private set; }
+    public int DeathTrapRow { get; private set; }
+    public int DeathTrapCol { get; private set; }
+}
