a356a820db7675bddd50dbb984df196122a50c4c
Tara Karimi <tara.karimi@icloud.com>
2023-10-14 00:25:08 +0330

    Create yiga foot soldier class

---
+++ Object Oriented Programming/BOTW/Zelda/Zelda/Enemy Types/YigaFootSoldier.cs
@@ -0,0 +1,33 @@
+﻿using Zelda.Properties;
+using System;
+
+namespace Zelda
+{
+    public class YigaFootSoldier: Enemy, ISneakable, IEatable, ISwimmable, ITeleportable
+    {
+        public YigaFootSoldier()
+            : base("Yiga Foot Soldier", "Male", "Sheikah", 150, 3, 12, new string[] {"Any"})
+        {
+        }
+        
+        public void Teleport()
+        {
+            Console.WriteLine("the Yiga Foot Soldier teleports to a different location");
+        }
+
+        public void SneakedOn()
+        {
+            Console.WriteLine("the Yiga Foot Soldier can be sneaked on");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("the Yiga Foot Soldier is eating");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("the Yiga Foot Soldier swims");
+        }
+    }
+}
