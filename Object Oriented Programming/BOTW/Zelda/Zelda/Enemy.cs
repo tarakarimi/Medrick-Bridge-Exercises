3aa7627e562cb2301d424acb0205c8b8dd3284f5
Tara Karimi <tara.karimi@icloud.com>
2023-10-13 23:43:59 +0330

    Add constructor with default values to enemy class

--- Object Oriented Programming/BOTW/Zelda/Zelda/Enemy.cs
+++ Object Oriented Programming/BOTW/Zelda/Zelda/Enemy.cs
@@ -8,5 +8,17 @@ public class Enemy
     public int Health { get; private set; }
     public int Threat { get; private set; } // out of 10
     public int Damage { get; private set; }
-    public virtual string[] EffectiveWeapons { get; }
+    public virtual string[] EffectiveWeapons { get; } 
+    
+    public Enemy(string name = "Unknown", string gender = "Unknown", string race = "Unknown", int health = 100, 
+                int threat = 10, int damage = 10, string[] effectiveWeapons = null)
+    {
+        Name = name;
+        Gender = gender;
+        Race = race;
+        Health = health;
+        Threat = threat;
+        Damage = damage;
+        EffectiveWeapons = effectiveWeapons ?? Array.Empty<string>();
+    }
 }
