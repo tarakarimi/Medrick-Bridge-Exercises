f9a2bd2447b1ac4c2d431b4e8b435ce0343ba9bf
Tara Karimi <tara.karimi@icloud.com>
2023-10-13 23:43:04 +0330

    Add enemy class properties

---
+++ Object Oriented Programming/BOTW/Zelda/Zelda/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Enemy
+{
+    public string Name { get; private set; }
+    public string Gender { get; private set; }
+    public string Race { get; private set; }
+    public int Health { get; private set; }
+    public int Threat { get; private set; } // out of 10
+    public int Damage { get; private set; }
+    public virtual string[] EffectiveWeapons { get; }
+}
